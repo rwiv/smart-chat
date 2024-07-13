fe42c2baf539b4c30b088ca73af747c60f2bb4bb
rwiv <zc798546@gmail.com>
2024-07-13 18:12:09 +0900

    first commit

---
+++ src/test/kotlin/com/github/smartchat/SmartChatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.smartchat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartChatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
